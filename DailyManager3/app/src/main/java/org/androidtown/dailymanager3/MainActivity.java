7fded3520df3e1aafac6c7a7ec00b51e4bc6a902
augleih <35728092+augleih@users.noreply.github.com>
2018-05-16 16:33:38 +0900

    diary 1
    home / main page of the diary

---
+++ DailyManager3/app/src/main/java/org/androidtown/dailymanager3/MainActivity.java
@@ -0,0 +1,26 @@
+package org.androidtown.dailymanager3;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable(){
+            public void run() {
+                Intent homeIntent = new Intent(getApplicationContext(),
+                        HomeActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+    }
+}
